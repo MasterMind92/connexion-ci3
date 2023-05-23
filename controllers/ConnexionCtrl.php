1fc270a833c9923c92ff142e6b677ccfd36fd45b
Marc <marc.dalo@sycapay.com>
2023-05-23 17:07:51 +0000

    initial commit

---
+++ controllers/ConnexionCtrl.php
@@ -0,0 +1,143 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ConnexionCtrl extends CI_Controller {
+
+	function __construct()
+	{	
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+		$this->load->model('Users_model','user');
+	}
+
+	public function index()
+	{
+		
+		// execution fonction de validation
+		$this->login_form_validation_request();
+
+		// si la validation ne se passe pas bien
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('partials/head');
+			$this->load->view('connexion/login');
+			$this->load->view('partials/foot');
+		}
+		// si la validation se passe bien
+		else
+		{	
+			var_dump($_POST);
+			// demander si l'utilisateur existe
+			// demander si c'est le bon login + mot de passe
+			$login = $this->input->post('login');
+			$mdp = $this->input->post('password');
+
+			$user = $this->user->get(NULL,NULL,NULL,$login,$mdp);
+
+			var_dump($user==NULL,sha1($mdp));
+
+			if ($user==NULL) {
+				// sinon on le renvoie a la page de connexion
+				// avec message d'erreur 
+				// login ou mot de passe incorrecte
+				$this->session->set_flashdata('msg', 'Login ou mot de passe incorrecte');
+
+				$this->load->view('partials/head');
+				$this->load->view('connexion/login');
+				$this->load->view('partials/foot');
+
+			} else {
+				
+				echo "Utilisateur authentifié avec succes \n";
+				echo "Page succes";
+				// redirect('');
+			}
+			
+			// si c'est bon on le connecte
+			// page de succes
+			// $this->load->view('formsuccess');
+		}
+	}
+
+	public function reinitialisation()
+	{
+		$this->load->view('partials/head');
+		$this->load->view('connexion/reinitialisation');
+		$this->load->view('partials/foot');
+	}
+
+	// vue de reinitialisation de l'utilisateur
+	public function inscription()
+	{
+		$this->load->view('partials/head');
+		$this->load->view('connexion/inscription');
+		$this->load->view('partials/foot');
+	}
+
+	// operation de validation d'un utilisateur qui tente de se connecter
+	public function validate_user()
+	{
+		// execution fonction de validation
+		$this->login_form_validation_request();
+
+		// si la validation ne se passe pas bien
+		if ($this->form_validation->run() == FALSE)
+		{
+			// page + erreurs
+			// $this->load->view('myform');
+			echo "Echec authentification utilisateur ";
+
+		}
+		// si la validation se passe bien
+		else
+		{	
+			// demander si l'utilisateur existe
+			var_dump($_POST);
+
+			// demander si c'est le bon login + mot de passe
+			// si c'est bon on le connecte
+			// sinon on le renvoie a la page de connexion
+			// avec message d'erreur 
+			// login ou mot de passe incorrecte
+			echo "Utilisateur authentifié avec succes";
+			// page de succes
+			// $this->load->view('formsuccess');
+		}
+	}
+
+	// operation validation du formulaire de connexion
+	public function login_form_validation_request()
+	{
+		$this->form_validation->set_rules('login', 'Login', 'required|valid_email',
+			array(
+				'required' => 'le champ %s est obligatoire.',
+				'alpha_numeric'=> 'le champ %s doit contenir que des chiffres et des lettres',
+				'valid_email'=> 'le champ %s n\'est pas une adresse mail valide',
+			)
+		);
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|trim|alpha_numeric',
+				array(
+					'required' => 'le champ %s est obligatoire.',
+					'min_length'=> 'le champ %s est inférieur à 8 caractères',
+					'alpha_numeric'=> 'le champ %s doit contenir que des chiffres et des lettres',
+				)
+		);
+
+		// $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+		// $this->form_validation->set_rules('email', 'Email', 'required');
+	}
+
+	// operation 
+	public function user_connected()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function redirect()
+	{
+		$this->load->view('welcome_message');
+	}
+
+
+}
---
+++ models/User_model.php
@@ -0,0 +1,180 @@
+<?php
+
+class User_model extends CI_Model {
+
+    // CRUD
+    protected $table;
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->table = "users";
+    }
+    
+    /**
+     * Creer un utilisateur
+     *
+     * @param string $nom
+     * @param string $prenoms
+     * @param string $mail
+     * @param string $phone
+     * @param string $login
+     * @param string $mdp
+     * @param string $role
+     * 
+     * @return boolean
+     * 
+     */
+    public function add($array)
+    {   
+        $this->db->set('nom', $array['nom']);
+        $this->db->set('prenoms', $array['prenoms']);
+        $this->db->set('mail', $array['email']);
+        $this->db->set('phone', $array['tel']);
+        $this->db->set('mdp', sha1($array['mdp']));
+        $this->db->set('role', $array['role']);
+        $this->db->set('etat_user   ', 'A');
+        $this->db->set('created_at', date('Y-m-d h:i:s'));
+        $this->db->set('updated_at', date('Y-m-d h:i:s'));
+
+        return $this->db->insert('users');
+    }
+
+
+    /**
+     * Modifier caractéristiques utilisateur
+     *
+     * @param int $id
+     * @param string $etat ['A','S','I']
+     * 
+     * @return boolean
+     * 
+     */
+    public function update_etat($id,$etat)
+    {
+        $this->db->set('etat', $etat);
+        $this->db->where('id', $id);
+
+        return $this->db->update('users');
+    }
+
+    
+    /**
+     * Rechercher un utilisateur
+     *
+     * @param mixed $date_debut
+     * @param mixed $date_fin
+     * 
+     * @return object
+     * 
+     */
+    public function search_users($date_debut,$date_fin)
+    {
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('created_at >=',$date_debut );
+        $this->db->where('created_at <=',$date_fin);
+
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+
+    /**
+     * Lister les utilisateurs
+     *
+     * @return object
+     * 
+     */
+    public function get_all_users()
+    {
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('etat_user !=','S');
+
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+
+
+    /**
+     * Rechercher un utiliateur specifique
+     *
+     * @param mixed $email
+     * @param mixed $pass
+     * 
+     * @return object
+     * 
+     */
+    public function get($email,$pass)
+    {   
+
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('mail',$email);
+        $this->db->where('mdp',sha1($pass));
+        
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+
+
+    /**
+     * Supprimer un utilisateur
+     *
+     * @param int $id
+     * 
+     * @return boolean
+     * 
+     */
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->set('deleted_at', date('Y-m-d h:i:s'));
+        $this->db->set('etat','S');
+
+        return $this->db->update('users');
+    }
+
+
+    // verification des etats
+    public function is_such_state($id, $etat)
+    {
+        $this->db->where('id',$id);
+        $this->db->where('etat',$etat);
+        $query = $this->db->get('users');
+        
+        return $query->result();
+    }
+
+    // state setter
+    public function set_state($id, $etat)
+    {
+        $this->db->set('etat',$etat);
+        $this->db->where('id',$id);
+        
+        return $this->db->update('users');
+    }
+
+    // role setter
+    public function set_role($id, $role)
+    {
+        $this->db->set('etat',$etat);
+        $this->db->where('role',$role);
+        
+        return $this->db->update('users');
+    }
+
+    // password setter
+    public function set_password($id, $pass)
+    {
+        $this->db->set('id',$id);
+        $this->db->where('role',sha1($pass));
+        
+        return $this->db->update('users');
+    }
+    
+}
